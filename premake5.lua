4597c77870acdae889d5c7e4107bc08be5e64df2
Matyassin <115926707+Matyassin@users.noreply.github.com>
2025-10-02 16:45:38 +0200

    Add files via upload

---
+++ premake5.lua
@@ -0,0 +1,25 @@
+project "ImGUI"
+    kind "StaticLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputDir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "immgui_internal.h",
+        "immgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++20"
+        staticruntime "On"
