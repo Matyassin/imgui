9af0d24ad9d4131252c93d97610afd77b3450254
Mátyás Végh <veghmatya@gmail.com>
2025-10-03 00:06:06 +0200

    Added ImGui

--- premake5.lua
+++ premake5.lua
@@ -11,12 +11,13 @@ project "ImGUI"
         "imgui.h",
         "imgui.cpp",
         "imgui_draw.cpp",
-        "immgui_internal.h",
-        "immgui_widgets.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
         "imstb_rectpack.h",
         "imstb_textedit.h",
         "imstb_truetype.h",
-        "imgui_demo.cpp"
+        "imgui_demo.cpp",
+        "imgui_tables.cpp"
     }
 
     filter "system:windows"
